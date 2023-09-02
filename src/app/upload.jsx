c8cc12f6a9120874158f07e4b8af05d614717f37
Raqeeb <raqeeb@Raqeebs-MacBook-Pro.local>
2023-09-03 00:06:51 +0500

    first commit

---
+++ src/app/upload.jsx
@@ -0,0 +1,51 @@
+// components/UploadButton.js
+import React, { useRef } from 'react';
+
+function UploadButton() {
+    const fileInputRef = useRef(null);
+
+    const handleFileUpload = () => {
+        fileInputRef.current.click();
+    };
+
+    const handleFileSelected = (e) => {
+        const selectedFile = e.target.files[0];
+        if (selectedFile) {
+            // Handle the selected file here
+            console.log('Selected file:', selectedFile);
+        }
+    };
+
+    return (
+        <div >
+            <button
+                onClick={handleFileUpload}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+                {/*<span className="mr-2">Upload File</span>*/}
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 inline-block"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M12 4v16m8-8H4"
+                    />
+                </svg>
+            </button>
+            <input
+                ref={fileInputRef}
+                type="file"
+                className="hidden"
+                onChange={handleFileSelected}
+            />
+        </div>
+    );
+}
+
+export default UploadButton;
